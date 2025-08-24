379699de137d2b4c9df119564af9f625fa64f152
Mankgarube Frederick <mfmafolo@outlook.com>
2025-08-24 20:58:50 +0200

    added class

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,68 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+
+function createEmployee(salary: number): Director | Teacher {
+  if (salary < 500) {  // Type assertion
+    return new Teacher();
+  }
+  return new Director();
+}
+
+// Type predicate function to check if employee is a Director
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Function to execute the appropriate work tasks
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Test cases
+console.log(executeWork(createEmployee(200)));   // Output: "Getting to work"
+console.log(executeWork(createEmployee(1000)));  // Output: "Getting to director tasks"
+
